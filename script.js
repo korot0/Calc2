63fbe078f3f2dd41c810568ac3b0de1570021a92
jt <108902180+korot0@users.noreply.github.com>
2024-07-03 20:49:46 -0500

    Create functions for populating display

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
-function operate(operator, num1, num2) {
-   
-    let result;
+// Output
+const output = document.querySelector("#output");
+// Delete buttons
+const clearBtn = document.querySelector("#clear-btn");
+const backspaceBtn = document.querySelector("#backspace-btn");
+// Operators
+const operators = document.querySelectorAll(".operators");
+// Numbers
+const numbers = document.querySelectorAll(".numbers");
+// Dot button
+const dotBtn = document.querySelector("#dot-btn");
 
-    switch(operator) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0) return "CANNOT DIVIDE BY ZERO";
-            result = num1 / num2;
-            break;
-        default:
-            // code block
+// Event listener for clearing output
+clearBtn.addEventListener("click", () => {
+    output.textContent = "0";
+});
+
+// Event listener for deleting digit in output
+backspaceBtn.addEventListener("click", () => {
+    if (output.textContent.length !== 1) {
+    output.textContent = output.textContent.slice(0, -1); 
+    } else {
+        output.textContent = "0";
     }
-    return result;
+});
+
+let notPopulated = true;
+
+// Display respective number
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if (notPopulated) {
+            output.textContent = "";
+            output.textContent += number.textContent;
+            notPopulated = false;
+        } else {
+            if (output.textContent.length !== 10) {
+            output.textContent += number.textContent;
+            }
+        } 
+    });
+});
+
+// Check that thing you were thinking about creating objects such as event listeners and query selectors affecting performance.
+
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        notPopulated = true;
+    });
+});
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case 'add':
+            return parseFloat(a) + parseFloat(b);
+        case 'subtract':
+            return parseFloat(a) - parseFloat(b);
+        case 'multiply':
+            return parseFloat(a) * parseFloat(b);
+        case 'divide':
+            if (parseFloat(b) === 0) return ('Cannot divide by zero');
+            return parseFloat(a) / parseFloat(b);
+        default:
+            // throw new Error('Invalid operator: &{operation}');
+    }
 }
 
-console.log(operate('/', 1, 0));
+console.log(operate('/', 1, 1));
